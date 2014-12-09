4b7dd321323835dd4e1a846711b95ed79b695270
Ryan Donegan <r.donegan44@gmail.com>
2014-12-09 14:16:42 -0500

    this is hopefully working

--- server.js
+++ server.js
@@ -1,24 +1,45 @@
-var express = require("express");
-var app = express();
-var morgan = require('morgan');
-var route = require('./routes/serverSocket.js');
+var express = require('express'),
+    morgan  = require('morgan'),
+    path = require('path');
 
-// Log requests
-app.use(morgan('tiny'));
+// Create a class that will be our main application
+var SimpleStaticServer = function() {
 
-// load static pages
-app.use(express.static(__dirname + '/public'));
+  // set self to the scope of the class
+  var self = this;  
+  
+  /*  ================================================================  */
+  /*  App server functions (main app logic here).                       */
+  /*  ================================================================  */
 
-//initialize socket.io
-var httpServer = require('http').Server(app);
-var sio =require('socket.io');
-var io = sio(httpServer);
+  self.app = express();
+  //	self.app.use(connect(connect.basicAuth('j', 'jmjm')))
+  self.app.use(morgan('[:date] :method :url :status'));	// Log requests
+  self.app.use(express.static(path.join(__dirname, 'public')));	// Process static files
 
-// httpServer.get('/', route.updateDB);
+  // Start the server (starts up the sample application).
+  self.start = function() {
+    /*
+     * OpenShift will provide environment variables indicating the IP 
+     * address and PORT to use.  If those variables are not available
+     * (e.g. when you are testing the application on your laptop) then
+     * use default values of localhost (127.0.0.1) and 33333 (arbitrary).
+     */
+    self.ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+    self.port      = process.env.OPENSHIFT_NODEJS_PORT || 50000;
 
-httpServer.listen(8000, function() {console.log('Listening on 50000');});
+    //  Start listening on the specific IP and PORT
+    self.app.listen(self.port, self.ipaddress, function() {
+      console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), self.ipaddress, self.port);
+    });
+  };
+}; 
 
 
-var serverSockets = require('./routes/serverSocket.js');
-serverSockets.init(io);
+/**
+ *  main():  Main code.
+ */
+var sss = new SimpleStaticServer();
+sss.start();
 
